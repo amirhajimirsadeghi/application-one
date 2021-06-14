0107c4134ecc804a22968b096117f791687221c3
Amir Hajimirsadeghi <amir@faliam.com>
2021-06-14 09:23:02 -0500

    fixing code

--- lib/application-one-pipeline-stack.ts
+++ lib/application-one-pipeline-stack.ts
@@ -1,43 +1,43 @@
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
 
 /**
  * The stack that defines the application pipeline
  */
 export class ApplicationOnePipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const sourceArtifact = new codepipeline.Artifact();
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
- 
-    const pipeline = new CdkPipeline(this, 'Pipeline', {
-      // The pipeline name
-      pipelineName: 'ApplicationOnePipeline',
-      cloudAssemblyArtifact,
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
 
-      // Where the source can be found
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
-        actionName: 'GitHub',
-        output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('github-token'),
-        owner: 'amirhajimirsadeghi',
-        repo: 'cdk-pipeline',
-      }),
+        const pipeline = new CdkPipeline(this, "Pipeline", {
+            // The pipeline name
+            pipelineName: "ApplicationOnePipeline",
+            cloudAssemblyArtifact,
 
-       // How it will be built and synthesized
-       synthAction: SimpleSynthAction.standardNpmSynth({
-         sourceArtifact,
-         cloudAssemblyArtifact,
-         
-         // We need a build step to compile the TypeScript Lambda
-         buildCommand: 'npm run build'
-       }),
-    });
+            // Where the source can be found
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: "GitHub",
+                output: sourceArtifact,
+                oauthToken: SecretValue.secretsManager("github-token"),
+                owner: "amirhajimirsadeghi",
+                repo: "application-one",
+            }),
 
-    // This is where we add the application stages
-    // ...
-  }
+            // How it will be built and synthesized
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+
+                // We need a build step to compile the TypeScript Lambda
+                buildCommand: "npm run build",
+            }),
+        });
+
+        // This is where we add the application stages
+        // ...
+    }
 }
